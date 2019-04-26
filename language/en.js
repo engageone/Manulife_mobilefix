f1a8c322b0d85eba0d97871e39205f8379bf9e8e
engageone <42962238+engageone@users.noreply.github.com>
2019-04-26 14:48:44 +0530

    Update en.js
    save

--- language/en.js
+++ language/en.js
@@ -45,14 +45,14 @@ var Translations = {
      */
     "CtaButtonSettings": {
         "button1": {
-            /*"card": "257522e9",
+            "card": "257522e9",
             "label": "Share",
-            "icon": "share"*/
+            "icon": "share"
         },
         "button2": {
-            /*"url": "https://www.manulifelabuan.com.my/Home",
+            "url": "https://www.manulifelabuan.com.my/Home",
             "label": "Website",
-            "icon": "website"*/
+            "icon": "website"
         },
         "button3": {
            /* "card": "a84f3919",
