9cc60b215d41f95f54d606ad24d37176c919c676
engageone <42962238+engageone@users.noreply.github.com>
2019-04-26 13:03:04 +0530

    Update es.js
    saved

--- language/es.js
+++ language/es.js
@@ -48,16 +48,16 @@ var Translations = {
             "label": "Botón 3",
             "icon": "email"
         },
-        "button4": {
+       /* "button4": {
             "card": "7b480c70",
-           /* "label": "Botón 4",*/
-           /* "icon": "save"*/
+            "label": "Botón 4",
+            "icon": "save"
         },
         "button5": {
             "url": "http://example.com/es",
-            /*"label": "Botón 5",*/
-           /* "icon": "share"*/
-        }
+            "label": "Botón 5",
+            "icon": "share"
+        }*/
     },
 
     /*
